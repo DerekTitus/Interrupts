d2527288f7c51d6b5c6a8f7b49bfe086073382d3
Derek Titus <c15derek.titus@usafa.edu>
2013-11-05 22:16:40 -0700

    put in cases for pushing the right and left buttons along with the resets if they go outside the boundary

--- workspace_v5_4/UtilizingInterrupts/Interrupts.c
+++ workspace_v5_4/UtilizingInterrupts/Interrupts.c
@@ -9,7 +9,7 @@
 
 #include <msp430g2553.h>
 #include "Interrupts.h"
-#include "LCD/LCD.h"
+
 
 unsigned char initPlayer()
 {
@@ -34,6 +34,36 @@ unsigned char movePlayer(unsigned char player, unsigned char direction)
                 //
                 // update player position based on direction of movement
                 //
+        case RIGHT
+        		clearPlayer(player);
+        			player += 1;
+        			if (player == 0x88)
+        			{
+        				player = 0x87; // 87 is the right limit/it cannot be 88
+        			}
+        			if (player ==0xC8)
+        			{
+        				player = 0xC7; //c7 is the right limit
+        			}
+        			printPlayer(player);
+        			waitForP1ButtonRelease(BIT1); //this function was taken from C2C Busho's code
+        			break;
+        case LEFT
+        		clearPlayer(player);
+        			player -= 1;
+        			if (player == 0x7F)
+        			{
+        				player = 0x80;
+        			}
+        			if (player == BF)
+        			{
+        				player = 0xC0;
+        			}
+        			printPlayer(player);
+        			waitForP1ButtonRelease(BIT2); //bit2 is connected to left button
+        			break;
+        case UP:
+        			}
         }
 
         return player;
